c4ff30b37c398bee14ae5b7eafeb98382c545533
Saltman0 <baudoin.mathieu@gmail.com>
2024-10-08 23:32:38 +0200

    Disconnect button function added

--- Views/HeaderControl.axaml.cs
+++ Views/HeaderControl.axaml.cs
@@ -1,4 +1,6 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using CinephoriaDesktop.Views.Login;
 
 namespace CinephoriaDesktop.Views;
 
@@ -7,5 +9,11 @@ public partial class HeaderControl : UserControl
     public HeaderControl()
     {
         InitializeComponent();
+    }
+
+    private void Disconnect(object? sender, RoutedEventArgs e)
+    {
+        var topLevel = TopLevel.GetTopLevel(this);
+        if (topLevel != null) topLevel.Content = new LoginControl();
     }
 }
