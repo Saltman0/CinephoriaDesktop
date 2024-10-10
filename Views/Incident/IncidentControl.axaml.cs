3d55c7c5f5b06b8499ba6948ed5e7a3c9aad2a69
Saltman0 <baudoin.mathieu@gmail.com>
2024-10-10 10:39:33 +0200

    IncidentControl added

---
+++ Views/Incident/IncidentControl.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace CinephoriaDesktop.Views.Incident;
+
+public partial class IncidentControl : UserControl
+{
+    public IncidentControl()
+    {
+        InitializeComponent();
+    }
+}
