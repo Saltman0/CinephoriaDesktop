944ad9db18d08144f830e8c7168c58291b6a84bc
Saltman0 <baudoin.mathieu@protonmail.com>
2025-01-08 23:55:30 +0100

    Update LoginControl.axaml.cs

--- Views/Login/LoginControl.axaml.cs
+++ Views/Login/LoginControl.axaml.cs
@@ -1,15 +1,23 @@
+using System;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
+using CinephoriaDesktop.Entities;
+using CinephoriaDesktop.Factory;
 using CinephoriaDesktop.Services;
 using CinephoriaDesktop.Views.Error;
+using CinephoriaDesktop.Views.Hall;
+using LiteDB;
 
 namespace CinephoriaDesktop.Views.Login;
 
 public partial class LoginControl : UserControl
 {
-    public LoginControl()
+    private readonly IApiService _apiService;
+    
+    public LoginControl(IApiService apiService)
     {
         InitializeComponent();
+        _apiService = apiService;
     }
     
     private void Connect(object? sender, RoutedEventArgs e)
@@ -21,13 +29,23 @@ public partial class LoginControl : UserControl
         }
         else
         {
-            if (!AuthentificationService.Authenticate(EmailTextBox.Text, PasswordTextBox.Text))
+            string result = _apiService.Authenticate(EmailTextBox.Text, PasswordTextBox.Text);
+
+            if (result == "error")
             {
                 WrongCredentialsErrorWindow wrongCredentialsErrorWindow = new WrongCredentialsErrorWindow();
                 wrongCredentialsErrorWindow.ShowDialog(TopLevel.GetTopLevel(this) as Window);
             }
+            
+            using (LiteDatabase database = new LiteDatabase(@"CinephoriaDesktop.db"))
+            {
+                ILiteCollection<JwtToken> jwtTokenCollection = database.GetCollection<JwtToken>("jwtTokens");
+                jwtTokenCollection.Insert(JwtTokenFactory.CreateJwtToken(result));
+                ILiteCollection<JwtToken> incidentTokenCollection = database.GetCollection<JwtToken>("incidents");
+            }
         }
         
         // TODO Display the Hall list window
+        Content = new HallListControl();
     }
 }
