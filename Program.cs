a6ffda01b2cea6c620193c981cfb6bce49db8481
Saltman0 <baudoin.mathieu@protonmail.com>
2025-01-08 23:55:25 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
 ﻿using Avalonia;
 using System;
+using CinephoriaDesktop.Services;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace CinephoriaDesktop;
 
@@ -9,8 +12,16 @@ class Program
     // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
     // yet and stuff might break.
     [STAThread]
-    public static void Main(string[] args) => BuildAvaloniaApp()
-        .StartWithClassicDesktopLifetime(args);
+    public static void Main(string[] args)
+    {
+        HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
+
+        builder.Services.AddTransient<IApiService, ApiService>();
+        
+        using IHost host = builder.Build();
+        
+        BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+    }
 
     // Avalonia configuration, don't remove; also used by visual designer.
     public static AppBuilder BuildAvaloniaApp()
