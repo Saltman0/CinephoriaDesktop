914b540596e03ad54c2259fe836dd1499ebda3e8
Saltman0 <baudoin.mathieu@protonmail.com>
2025-01-11 15:03:56 +0100

    Update LoginControl.axaml.cs

--- Views/Login/LoginControl.axaml.cs
+++ Views/Login/LoginControl.axaml.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
 using CinephoriaDesktop.Entities;
@@ -12,12 +13,9 @@ namespace CinephoriaDesktop.Views.Login;
 
 public partial class LoginControl : UserControl
 {
-    private readonly IApiService _apiService;
-    
-    public LoginControl(IApiService apiService)
+    public LoginControl()
     {
         InitializeComponent();
-        _apiService = apiService;
     }
     
     private void Connect(object? sender, RoutedEventArgs e)
@@ -29,23 +27,37 @@ public partial class LoginControl : UserControl
         }
         else
         {
-            string result = _apiService.Authenticate(EmailTextBox.Text, PasswordTextBox.Text);
+            string result = ApiService.Authenticate("http://172.18.0.6", EmailTextBox.Text, PasswordTextBox.Text);
 
             if (result == "error")
             {
                 WrongCredentialsErrorWindow wrongCredentialsErrorWindow = new WrongCredentialsErrorWindow();
                 wrongCredentialsErrorWindow.ShowDialog(TopLevel.GetTopLevel(this) as Window);
             }
-            
-            using (LiteDatabase database = new LiteDatabase(@"CinephoriaDesktop.db"))
+            else
             {
-                ILiteCollection<JwtToken> jwtTokenCollection = database.GetCollection<JwtToken>("jwtTokens");
-                jwtTokenCollection.Insert(JwtTokenFactory.CreateJwtToken(result));
-                ILiteCollection<JwtToken> incidentTokenCollection = database.GetCollection<JwtToken>("incidents");
+                string baseDirectory = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+                
+                string databasePath = Path.Combine(baseDirectory, "CinephoriaDesktop", "CinephoriaDesktop.db");
+                
+                LiteDatabase? cinephoriaDesktopDatabase = DatabaseService.GetDatabase("/home/saltman/Documents/Test/CinephoriaDesktop.db");
+
+                if (cinephoriaDesktopDatabase != null)
+                {
+                    Console.WriteLine("Database created successfully.");
+
+                    ILiteCollection<JwtToken> jwtTokenCollection = cinephoriaDesktopDatabase.GetCollection<JwtToken>("jwtTokens");
+                    jwtTokenCollection.Insert(JwtTokenFactory.Create(result));
+                }
+                else
+                {
+                    WrongCredentialsErrorWindow wrongCredentialsErrorWindow = new WrongCredentialsErrorWindow();
+                    wrongCredentialsErrorWindow.ShowDialog(TopLevel.GetTopLevel(this) as Window);
+                }
+                
+                // TODO Display the Hall list control
+                Content = new HallListControl();
             }
         }
-        
-        // TODO Display the Hall list window
-        Content = new HallListControl();
     }
 }
