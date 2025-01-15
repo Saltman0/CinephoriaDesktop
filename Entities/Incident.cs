fd559e3d44f3a523080c80c4a22f3030a08cd57b
Saltman0 <baudoin.mathieu@protonmail.com>
2025-01-15 18:24:05 +0100

    Update Incident.cs

--- Entities/Incident.cs
+++ Entities/Incident.cs
@@ -7,11 +7,13 @@ public class Incident
     private int _id;
     private string _type;
     private string _description;
+    private int _hallId;
 
-    public Incident(string type, string description)
+    public Incident(string type, string description, int hallId)
     {
         _type = type;
         _description = description;
+        _hallId = hallId;
     }
 
     public int Id
@@ -30,5 +32,11 @@ public class Incident
     {
         get => _description;
         set => _description = value ?? throw new ArgumentNullException(nameof(value));
+    }
+
+    public int HallId
+    {
+        get => _hallId;
+        set => _hallId = value;
     }
 }
