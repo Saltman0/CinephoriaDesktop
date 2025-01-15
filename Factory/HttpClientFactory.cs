486216bdc447814883476e216be0cdad38abe593
Saltman0 <baudoin.mathieu@protonmail.com>
2025-01-15 11:19:43 +0100

    Update HttpClientFactory.cs

--- Factory/HttpClientFactory.cs
+++ Factory/HttpClientFactory.cs
@@ -6,12 +6,16 @@ namespace CinephoriaDesktop.Factory;
 
 public static class HttpClientFactory
 {
-    public static HttpClient Create(string baseAddress)
+    public static HttpClient Create(string baseAddress, string? jwtToken)
     {
         HttpClient httpClient = new HttpClient();
         
         httpClient.BaseAddress = new Uri(baseAddress);
         httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        if (jwtToken != null)
+        {
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", jwtToken);
+        }
         
         return httpClient;
     }
