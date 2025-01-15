151fa153c6de3977645eec8e3f13bce584d51639
Saltman0 <baudoin.mathieu@protonmail.com>
2025-01-15 18:24:08 +0100

    Update IncidentFactory.cs

--- Factory/IncidentFactory.cs
+++ Factory/IncidentFactory.cs
@@ -4,8 +4,8 @@ namespace CinephoriaDesktop.Factory;
 
 public static class IncidentFactory
 {
-    public static Incident Create(string type, string description)
+    public static Incident Create(string type, string description, int hallId)
     {
-        return new Incident(type, description);
+        return new Incident(type, description, hallId);
     }
 }
